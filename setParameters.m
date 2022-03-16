16e166efe7d61b5a279e5ce4b438c8dd602fc9cd
marcobarilari <marcobarilr@gmail.com>
2022-03-16 23:06:58 +0100

    remove cfg input that are now defaults

--- setParameters.m
+++ setParameters.m
@@ -6,20 +6,21 @@ function [cfg] = setParameters()
 
     % Initialize the parameters and general configuration variables
     cfg = struct();
+    
+    cfg = checkCppPtbCfg(cfg);
 
     %% Debug mode settings
 
-    cfg.debug.do = true; % To test the script out of the scanner, skip PTB sync
     cfg.debug.smallWin = false; % To test on a part of the screen, change to 1
-    cfg.debug.transpWin = true; % To test with trasparent full size screen
-
-    cfg.verbose = 1;
+    cfg.debug.transpWin = false; % To test with trasparent full size screen
 
     %% Engine parameters
 
     cfg.testingDevice = 'mri';
     cfg.eyeTracker.do = false;
     cfg.audio.do = false;
+    
+    cfg.userIsExperimenter = 1;
 
     cfg = setMonitor(cfg);
 
@@ -27,20 +28,30 @@ function [cfg] = setParameters()
     cfg = setKeyboards(cfg);
 
     cfg.screen.effectiveFieldOfView = [0 0 800 600]; % in pixels
-
-    % Instruction
-    cfg.task.instruction = [ ...
-                            'Move and scale red rectangle to fill your field of view:\n\n', ...
-                            '  - Right hand:\n', ...
-                            '    - index: move right\n', ...
-                            '    - major: move left\n', ...
-                            '    - ring finger: move up\n', ...
-                            '    - little: move down\n\n', ...
-                            '  - Left hand:\n', ...
-                            '    - index: scale up\n', ...
-                            '    - major: scale down\n\n', ...
-                            'Let us know when you are done.'];
-
+    
+    if cfg.userIsExperimenter
+        
+        % Instruction
+        cfg.task.instruction = [ 'Guide the experimenter on how to move the red rectagnle \n', ...
+            'until is fully visible on the screen.' ];
+        
+    else 
+                
+        % Instruction
+        cfg.task.instruction = [ ...
+            'Move and scale red rectangle to fill your field of view:\n\n', ...
+            '  - Right hand:\n', ...
+            '    - index: move right\n', ...
+            '    - major: move left\n', ...
+            '    - ring finger: move up\n', ...
+            '    - little: move down\n\n', ...
+            '  - Left hand:\n', ...
+            '    - index: scale up\n', ...
+            '    - major: scale down\n\n', ...
+            'Let us know when you are done.'];
+        
+    end
+    
 end
 
 function cfg = setKeyboards(cfg)
@@ -49,25 +60,45 @@ function cfg = setKeyboards(cfg)
     cfg.keyboard.responseKey = { ...
                                 'r', 'g', 'y', 'b', ...
                                 'd', 'n', 'z', 'e', ...
+                                'LeftArrow', 'RightArrow', ...
+                                'UpArrow', 'DownArrow', ...
+                                'p', 'm', ...
                                 't', 'space'};
 
-    % Right hand
+                            
+    if cfg.userIsExperimenter             
+        
+        % move
+        cfg.keyboard.keyToMoveUp = 'UpArrow';
+        cfg.keyboard.keyToMoveDown = 'DownArrow';
+        cfg.keyboard.keyToMoveRight = 'RightArrow';
+        cfg.keyboard.keyToMoveLeft = 'LeftArrow';
 
-    % index
-    cfg.keyboard.keyToMoveUp = 'r';
-    % major
-    cfg.keyboard.keyToMoveDown = 'g';
-    % ring finger
-    cfg.keyboard.keyToMoveRight = 'y';
-    % little finger
-    cfg.keyboard.keyToMoveLeft = 'b';
+        %scale
+        cfg.keyboard.keyToScaleUp = 'p';
+        cfg.keyboard.keyToScaleDown = 'm';
+        
+    else 
+        
+        % Right hand (move)
 
-    % Left hand
-
-    % index
-    cfg.keyboard.keyToScaleUp = 'd';
-    % major
-    cfg.keyboard.keyToScaleDown = 'n';
+        % index
+        cfg.keyboard.keyToMoveUp = 'r';
+        % major
+        cfg.keyboard.keyToMoveDown = 'g';
+        % ring finger
+        cfg.keyboard.keyToMoveRight = 'y';
+        % little finger
+        cfg.keyboard.keyToMoveLeft = 'b';
+        
+        % Left hand
+        
+        % index
+        cfg.keyboard.keyToScaleUp = 'd';
+        % major
+        cfg.keyboard.keyToScaleDown = 'n';
+        
+    end
 
     cfg.keyboard.keyboard = [];
     cfg.keyboard.responseBox = [];
@@ -77,10 +108,7 @@ end
 function cfg = setMonitor(cfg)
 
     % Monitor parameters for PTB
-    cfg.color.white = [255 255 255];
-    cfg.color.black = [0 0 0];
-    cfg.color.red = [255 0 0];
-    cfg.color.grey = mean([cfg.color.black; cfg.color.white]);
+    cfg.color = cppPtbDefaults('color');
     cfg.color.background = cfg.color.black;
     cfg.text.color = cfg.color.white;
 
